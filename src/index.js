b566072780814170d32b31e6fb44179f5a8c4d70
Alexander Schetnikov <hi.sanyaaa@ya.ru>
2019-12-02 10:01:55 +0700

    ~(index) Подключено храналище (глобально)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "mobx-react";
 import App from './App'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import ApiService from './services/api.service'
+import store from './store'
 
 ApiService.init()
 
 ReactDOM.render(
+  <Provider {...store}>
     <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
-    document.getElementById('root')
+      <App/>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
